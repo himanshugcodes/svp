7e3355f3ac50b98c0ac0e74e9d1b1b7e2466b04d
Himanshu Gupta <108939032+himanshugcodes@users.noreply.github.com>
2024-12-31 00:04:54 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -31,7 +31,17 @@ export default function Home() {
             </div>
           </div>
         </section>
-
+        
+          <div className="row justify-content-center">
+              <div className="col-md-3">
+                <div className="text-center pt-3 scroll">
+                  <a href="/#main-down">
+                    <p>Scroll down <span className="arrow"><LiaAngleDoubleDownSolid /></span></p>
+                  </a>
+                </div>
+              </div>
+            </div>
+        
         <section className="partner section-padding">
           <div className="container">
             <div className="row justify-content-center">
